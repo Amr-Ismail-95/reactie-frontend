b6b95e7b349964688d97e53a68c8859a1f19da1b
Amr Ismail <eng.amr.ismail95@gmail.com>
2022-11-08 16:54:36 +0200

    implemented part of the login component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="1096416592530-fuj19tbdb8p91co301nb6pekri1h8n5f.apps.googleusercontent.com">
+  <Router>
     <App />
+  </Router>
+  </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
